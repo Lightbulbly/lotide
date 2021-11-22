12d496d265653d7c9cfb995bd72b188a7732ca6d
Noya Onggud <yanuo44@gmail.com>
2021-11-22 22:09:40 +0000

    eslint head.js

--- head.js
+++ head.js
@@ -1,5 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed ${actual} === ${expected} `);
   } else {
@@ -8,8 +8,10 @@ const assertEqual = function (actual, expected) {
 };
 
 // FUNCTION TO RETURN THE FIRST ELEMENT OF THE ARRAY
-const head = function (arr) {
-  if(arr.length===0){ return undefined;} else return arr[0];
+const head = function(arr) {
+  if (arr.length === 0) {
+    return undefined;
+  } else return arr[0];
 };
 
 //TEST CASE
