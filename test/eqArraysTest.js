aee59b3921fa6483e61cae94643ebc036071f74f
Noya Onggud <yanuo44@gmail.com>
2021-11-29 17:05:54 -0500

    wrote eqArraysTest.js

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -5,9 +5,9 @@ const assertEqual = _.assertEqual;
 const eqArrays = _.eqArrays;
 
 console.log(`
-#assertObjectsEqual-----starts here`);
-assertEqual(assertEqual([], []), true); // => should PASS;
+#eqArrays-----starts here`);
+assertEqual(eqArrays([], []), true); // => should PASS;
 assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false); // => should PASS
 assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 2, 4]), true); // => should FAIL
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-console.log(`#assertObjectsEqual-----ends here`);
+console.log(`#eqArrays-----ends here`);
