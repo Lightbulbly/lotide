bec123cd5eb1b910c7b850ff1394934fda319810
Noya Onggud <yanuo44@gmail.com>
2021-11-29 02:31:14 -0500

    Mocha and Chai middleTest and tailTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,30 @@
-const assertArraysEqual= require('../assertArraysEqual');
-const middle = require('../middle');
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2]), [1, 2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+const assert = require('chai').assert;
+const middle   = require('../middle');
+describe("#middle", () => {
+  it("returned empty array for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it(`returned empty array for [1]`, () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it(`returned [2] for [1,2,3]`, () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it(`returned [1, 2] for [1,2]`, () => {
+    assert.deepEqual(middle([1, 2]), [1, 2]);
+  });
+  it(`returned [2, 3] for [1, 2, 3, 4]`, () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it(`returned [4] for [1, 2, 3, 4, 5, 6, 7]`, () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+  });
+
+  it(`returned [4, 5] for [1, 2, 3, 4, 5, 6, 7, 8]`, () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+  });
+  
+});
+  
+  
+  
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,33 @@
-const assertEqual = require('../assertEqual');
-const tail =require('../tail');
-//TEST CASE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-//Make sure the original array is not modified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements
+describe("#tail", () => {
+  it("returned array's length is 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+  });
+  it(`returned array's index 0 is Lighthouse for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse");
+  });
+  it(`returned array's index 1 is Labs for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs");
+  });
+    
+  //Make sure the original array is not modified
+  it(`the original array is not modified for ["Yo Yo", "Lighthouse", "Labs"]`, () => {
+    let words = ["Yo Yo", "Lighthouse", "Labs"];
+    let array = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(array);
+    assert.deepEqual(array, ["Yo Yo", "Lighthouse", "Labs"]);
+  });
 
-//Test if tail function returns empty array when passed in an empty array
-console.log("empty arr: ", tail([]), tail([1]));
+  it(`returned array's index 1 is Labs for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs");
+  });
+    
+  it(`tail function returns empty array when passed in an empty array`, () => {
+    assert.deepEqual(tail([]), []);
+  });
 
-//Make sure the original array is not modified
-assertEqual();
+});
+
+
