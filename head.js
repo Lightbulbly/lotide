fd3c20c1a5440ffcca725ca470049929cbb0a3b1
Noya Onggud <yanuo44@gmail.com>
2021-11-29 00:21:36 -0500

    refactored assertEqual and head

--- head.js
+++ head.js
@@ -1,20 +1,7 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed ${actual} === ${expected} `);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // FUNCTION TO RETURN THE FIRST ELEMENT OF THE ARRAY
 const head = function(arr) {
   if (arr.length === 0) {
     return undefined;
   } else return arr[0];
 };
-
-//TEST CASE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports = head;
